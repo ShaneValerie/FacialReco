ff724077df533a5a3821560248916b3e48cfa19a
ShaneValerie <24104801@usc.edu.ph>
2026-07-10 00:28:17 +0800

    Delete face_module directory

--- face_module/db.php
+++
@@ -1,9 +0,0 @@
-<?php
-$conn = new mysqli("localhost", "root", "", "tenant_mgmt");
-if ($conn->connect_error) {
-    http_response_code(500);
-    die(json_encode(["error" => "DB connection failed"]));
-}
-?>
-
-
